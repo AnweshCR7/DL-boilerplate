692232af19e47d0a22ff10abe7fb2359afc6ccf8
Anwesh Marwade <anwesh.marwade@beyondsports.nl>
2021-01-03 22:30:06 +0100

    First Push (working model)

---
+++ src/config.py
@@ -0,0 +1,15 @@
+# DATA_DIR = "/content/drive/MyDrive/data/captcha"
+# CHECKPOINT_PATH = "/content/drive/MyDrive/data/checkpoint"
+# PLOT_PATH = "/content/drive/MyDrive/data/plots"
+# NUM_WORKERS = 1
+# DEVICE = "gpu"
+
+DATA_DIR = "../data/"
+CHECKPOINT_PATH = "../checkpoint"
+PLOT_PATH = "../plots"
+BATCH_SIZE = 8
+IMAGE_WIDTH = 300
+IMAGE_HEIGHT = 75
+NUM_WORKERS = 0
+DEVICE = "cpu"
+EPOCHS = 2
